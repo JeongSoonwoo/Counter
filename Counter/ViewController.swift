919057ecd16137574029ab4fc0e85f04717096d8
정순우 <soonwoo2714@gmail.com>
2018-03-20 15:30:37 +0900

    upload Counter

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    var value = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("\(value) button pressed!")
+        value = value + 1
+        myLabel.text = String(value)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
